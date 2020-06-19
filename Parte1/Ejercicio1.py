7acf60cca8e5770d6fa3df87f5801138f8c807d3
Emily <emily.moraima.cruz@hotmail.com>
2020-06-19 12:57:27 -0400

    Mayor de dos numeros

---
+++ Parte1/Ejercicio1.py
@@ -0,0 +1,10 @@
+def max(num1,num2):
+    if num1>num2:
+        print(f"El numero mayor es el {num1}")
+    elif num2>num1:
+        print(f"El numero mayor es el {num2}")
+    else:
+        print("Los dos numeros son iguales")
+
+
+max(5,8)

7bd1f362b112add8840d8935eec9725637d4ae4f
Marcos <23510012+MarcosRugerio@users.noreply.github.com>
2020-06-24 18:06:09 -0500

    Actualizando identación

--- Parte1/Ejercicio2.py
+++ Parte1/Ejercicio2.py
@@ -9,11 +9,9 @@ que tome tres números como argumentos y
 
 
 def max_de_tres():
-     
-num1 = input ("Escribe tu primer número: \n") 
-num2 = input ("Escribe tu segundo número: \n") 
-num3 = input ("Escribe tu tercer número: \n")
-
-if num1 > num2 num3 # Necesito operador para un OR para evaluar el mayor. Buscando 
+ num1 = input ("Escribe tu primer número: \n")
+ num2 = input ("Escribe tu segundo número: \n")
+ num3 = input ("Escribe tu tercer número: \n")
+ if num1 > num2 num3 # Necesito operador para un OR para evaluar el mayor. Buscando 
 
 max_de_tres()

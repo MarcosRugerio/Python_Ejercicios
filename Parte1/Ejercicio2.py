50306f2766aa76f2c79d985f94d223c2a66d3c17
Marcos Rugerio <marcosrugerio@gmail.com>
2020-06-26 10:09:33 -0500

    Formato del código

--- Parte1/Ejercicio2.py
+++ Parte1/Ejercicio2.py
@@ -19,8 +19,8 @@ def max_de_tres(numero1,numero2,numero3):
     print(numeroMayor)
 
 print("Este programa compara tres números y muestra el mayor de ellos\n")
-numero1 = input ("\nEscribe tu primer número: ")
-numero2 = input ("Escribe tu segundo número: ")
-numero3 = input ("Escribe tu tercer número: ")
+numero1 = int(input ("\nEscribe tu primer número: "))
+numero2 = int(input ("Escribe tu segundo número: "))
+numero3 = int(input ("Escribe tu tercer número: "))
 
 max_de_tres(numero1,numero2,numero3)

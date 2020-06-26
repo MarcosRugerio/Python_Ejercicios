9bec69be14ee076405d0385bcc7ce48278ff70be
Marcos Rugerio <marcosrugerio@gmail.com>
2020-06-25 19:59:15 -0500

    Comparando tres numeros. Me costó trabajo LOL

--- Parte1/Ejercicio2.py
+++ Parte1/Ejercicio2.py
@@ -1,17 +1,26 @@
-'''
-Ejercicio 2
+# '''
+# Ejercicio 2
+# Definir una función max_de_tres(), 
+# que tome tres números como argumentos y
+#  devuelva el mayor de ellos.
+#'''
+def max_de_tres(numero1,numero2,numero3):
+    print("El número mayor es:") #Validand que se llame correctamente.
+    if numero1 >= numero2:
+        if numero1 >= numero3:
+            numeroMayor = numero1
+        else:
+            numeroMayor = numero3
+    else:
+        if numero2 >= numero3:
+            numeroMayor = numero2
+        else:
+            numeroMayor = numero3
+    print(numeroMayor)
 
-Definir una función max_de_tres(), 
-que tome tres números como argumentos y
- devuelva el mayor de ellos.
+print("Este programa compara tres números y muestra el mayor de ellos\n")
+numero1 = input ("\nEscribe tu primer número: ")
+numero2 = input ("Escribe tu segundo número: ")
+numero3 = input ("Escribe tu tercer número: ")
 
- '''
-
-
-def max_de_tres():
- num1 = input ("Escribe tu primer número: \n")
- num2 = input ("Escribe tu segundo número: \n")
- num3 = input ("Escribe tu tercer número: \n")
- if num1 > num2 num3 # Necesito operador para un OR para evaluar el mayor. Buscando 
-
-max_de_tres()
+max_de_tres(numero1,numero2,numero3)
